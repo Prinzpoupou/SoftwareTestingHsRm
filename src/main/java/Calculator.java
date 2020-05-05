1d3bb612cb9a2ea6a826bc9c0788f0efbae4c642
Puri <pouria.hosseini@committance.com>
2020-05-05 21:05:23 +0200

    die Eingabe von Bash wurde hinzugefügt, um weitere Testfälle zu erschaffen

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Calculator {
@@ -21,25 +22,65 @@ public class Calculator {
         return a/b;
     }
 
-    public int add(int a, int b) {
-        return a+b;
-    }
-
-    public int sub(int a, int b) {
-        return a-b;
-    }
-
-    public int multi(int a, int b) {
-        return a*b;
-    }
-
-    public int div(int a, int b) {
-        if(b==0){
-            throw new ArithmeticException();
-        }
-        return a/b;
-    }
-
     public Calculator(){}
+
+    public String input(){
+        System.out.println("Bitte geben Sie ein String in form von '1+2' an:");
+        Scanner sc = new Scanner(System.in);
+        String userInput = "";
+        userInput = sc.next();
+
+        userInput = userInput.trim();
+        ArrayList arr = new ArrayList();
+        arr.add(userInput.contains("+"));
+        arr.add(userInput.contains("-"));
+        arr.add(userInput.contains("/"));
+        arr.add(userInput.contains("*"));
+
+        int functiondef = -1;
+        for(int i = 0; i < arr.size(); i++){
+            if((boolean) arr.get(i)){
+                functiondef = i;
+                break;
+            }
+        }
+
+        switch (functiondef) {
+            case 0:{
+                String[] parts = userInput.split("\\+");
+                double a =Double.parseDouble(parts[0]);
+                double b =Double.parseDouble(parts[1]);
+                double res = add(a, b);
+                return Double.toString(res);}
+            case 1:{
+                String[] parts = userInput.split("-");
+                double a =Double.parseDouble(parts[0]);
+                double b =Double.parseDouble(parts[1]);
+                double res = sub(a, b);
+                return Double.toString(res);}
+            case 2:{
+                String[] parts = userInput.split("/");
+                double a =Double.parseDouble(parts[0]);
+                double b =Double.parseDouble(parts[1]);
+                double res = div(a, b);
+                return Double.toString(res);}
+            case 3:{
+                String[] parts = userInput.split("\\*");
+                double a =Double.parseDouble(parts[0]);
+                double b =Double.parseDouble(parts[1]);
+                double res = multi(a, b);
+                return Double.toString(res);}
+            case -1:
+                throw new IllegalArgumentException();
+            default:
+                return "Falschen Operator eingegeben";
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.input());
+    }
 
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,6 +3,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
@@ -18,53 +21,13 @@ class CalculatorTest {
     }
 
     @ParameterizedTest
-    @CsvSource({"1,2,3", "0,2,2", "-1,2,1", "-1,-1,-2"})
+    @CsvSource({"1,2,3", "0,2,2", "-1,2,1", "-1,-1,-2", "1.1,2,3.1", "0.0,2,2.0", "-1.1,2.2,1.1", "-1,-1,-2"})
     void addInt(ArgumentsAccessor argumentsAccessor) {
-        int a = argumentsAccessor.getInteger(0);
-        int b = argumentsAccessor.getInteger(1);
-        int res = argumentsAccessor.getInteger(2);
-
-        assertEquals(res, calculator.add(a,b));
-    }
-
-    @ParameterizedTest
-    @CsvSource({"1.1,2,3.1", "0.0,2,2.0", "-1.1,2.2,1.1", "-1,-1,-2"})
-    void addDouble(ArgumentsAccessor argumentsAccessor) {
         double a = argumentsAccessor.getDouble(0);
         double b = argumentsAccessor.getDouble(1);
         double res = argumentsAccessor.getDouble(2);
 
         assertEquals(res, calculator.add(a,b));
-    }
-
-    @Test
-    void addInt() {
-        assertEquals(5, calculator.add(2, 3));
-        int bla = calculator.add(2, 3);
-        assertEquals(5, calculator.add(2, 3));
-        assertEquals(5.111111111111111, calculator.add(2.111111111111111111111111111, 3));
-    }
-
-    @Test
-    void subInt() {
-        assertEquals(-1, calculator.sub(2, 3));
-    }
-
-    void subIntNeg() {
-        assertEquals(-1, calculator.sub(2, 3));
-    }
-
-    @Test
-    void multiInt() {
-        assertEquals(6, calculator.multi(2, 3));
-    }
-
-    @Test
-    void divInt() {
-        assertEquals(2, calculator.div(2, 1));
-        assertThrows(ArithmeticException.class, ()->{
-            calculator.div(3,0);
-        });
     }
 
     @Test
@@ -90,4 +53,39 @@ class CalculatorTest {
             calculator.div(3,0);
         });
     }
+
+    @ParameterizedTest
+    @CsvSource({"5.01+2.000,7.01", "5.01-2.000,3.01", "5.01/2.000,2.505", "5.01*2.000,10.02", "12+-2,10.0"})
+    public void shouldTakeUserInput(ArgumentsAccessor argumentsAccessor) {
+        String input = argumentsAccessor.getString(0);
+        String output = argumentsAccessor.getString(1);
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertEquals(output, calculator.input());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"5.01+k23", "5.01-2.j0", "+23"})
+    public void schoulThrowError(ArgumentsAccessor argumentsAccessor) {
+        String input = argumentsAccessor.getString(0);
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertThrows(IllegalArgumentException.class, ()->{
+            System.setIn(in);
+            calculator.input();
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource({"12--2", "12*-2"})
+    public void schoulThrowErrorOutOfBounce(ArgumentsAccessor argumentsAccessor) {
+        String input = argumentsAccessor.getString(0);
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertThrows(NumberFormatException.class, ()->{
+            System.setIn(in);
+            calculator.input();
+        });
+    }
+
 }
